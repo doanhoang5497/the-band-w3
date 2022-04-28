c88cdc0833269f24cc90f5d232ff655ab5f46f2e
Đoàn Hoàng <doanhoang5497@gmail.com>
2022-04-28 17:22:27 +0700

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+
+var counter = 1;
+if (counter <= 3) {
+    setInterval(() => {
+        document.getElementById('Slider').style.background = `url('/assets/image/slider${counter}.jpg') top center / cover no-repeat`
+        switch (counter) {
+            case 1:
+                document.getElementsByClassName('Slider__txt-heading')[0].innerHTML = "New York"
+                document.getElementsByClassName('Slider__txt-content')[0].innerHTML = "The atmosphere in New York is lorem ipsum."
+                break;
+            case 2:
+                document.getElementsByClassName('Slider__txt-heading')[0].innerHTML = "Chicago"
+                document.getElementsByClassName('Slider__txt-content')[0].innerHTML = "Thank you, Chicago - A night we won't forget."
+
+                break;
+            case 3:
+                document.getElementsByClassName('Slider__txt-heading')[0].innerHTML = "Los Angeles"
+                document.getElementsByClassName('Slider__txt-content')[0].innerHTML = "We had the best time playing at Venice Beach!"
+
+                break;
+            default:
+                document.getElementsByClassName('Slider__txt-heading')[0].innerHTML = "New York"
+                document.getElementsByClassName('Slider__txt-content')[0].innerHTML = "The atmosphere in New York is lorem ipsum."
+                break;
+        }
+        counter++;
+        if (counter === 4) {
+            counter = 1;
+        }
+    }, 3000)
+}
